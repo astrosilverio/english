a8d266dfd269c6da4c4fa34091945dd443ab7941
Katie Silverio <astrosilverio@gmail.com>
2013-11-14 17:31:53 -0500

    made some demos

---
+++ demo.py
@@ -0,0 +1,20 @@
+def f(x):
+	x = x * x
+	return x + 1
+
+
+if __name__ == '__main__':
+	import english
+	from subprocess import Popen
+	
+	print "\nFunction:"
+	print "\ndef f(x)\n    x = x * x\n    return x + 1"
+	a = english.EnglishPython(f)
+	print "\nBytecode:"
+	print a.bytecode
+	print "Translation:\n"
+	a.print_english()
+	print "\n"
+	p = Popen(['say', a.english_string()])
+	
+	
---
+++ demo2.py
@@ -0,0 +1,17 @@
+def g(a, b, c):
+	x = add(a, multiply(b, foo(c, 42)))
+	return x
+
+if __name__ == '__main__':
+	import english
+	from subprocess import Popen
+
+	print "\nFunction:"
+	print "\ndef g(x)\n    x = add(a, multiply(b, foo(c, 42)))\n    return x"
+	a = english.EnglishPython(g)
+	print "\nBytecode:"
+	print a.bytecode
+	print "Translation:\n"
+	a.print_english()
+	print "\n"
+	p = Popen(['say', a.english_string()])
