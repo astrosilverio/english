79612bc15775c186ea579b4bd1e9543ab5f3402b
Katie Silverio <astrosilverio@gmail.com>
2013-11-21 11:36:04 -0500

    generalized pushes and pops

--- fakerun.py
+++ fakerun.py
@@ -8,6 +8,8 @@ class FakeRun(object):
     """
     Class that "fake" runs the bytecode and create the translation during it.
     """
+    tupledict = {'load': (0,1,0), 'store': (1,0,1), 'ret': (1,0,1), 'binary': (2,1,1),
+                'compare': (2,1,0)}
 
     def __init__(self, f):
         self.func = f
@@ -35,8 +37,9 @@ class FakeRun(object):
 #        popped = self.pythonstack[-pops:]
 #        self.pythonstack = self.pythonstack[:-pops]
 #        return popped
-
+# Tuple order: (pop, append_to_python, append_to_english)
     def call(self, num_args):
+    	#(num_args+1,1,1)
         f_and_args = map(lambda x: x.short, self._python_stack[-(num_args+1):])
         self._python_stack = self._python_stack[:-(num_args+1)]
         call_string = EnglishByte('call', *f_and_args)
@@ -44,20 +47,24 @@ class FakeRun(object):
         self.english_stack.append(call_string)
 
     def load(self, variable):
+    	# (0,1,0)
         load_string = EnglishByte('load', variable)
         self._python_stack.append(load_string)
 
     def store(self, variable):
+    	# (1,0,1)
         value = self._python_stack.pop().short
         store_string = EnglishByte('store', variable, value)
         self.english_stack.append(store_string)
 
     def ret(self, a_none):
+    	# (1,0,1)
         value = self._python_stack.pop().short
         ret_string = EnglishByte('return', value)
         self.english_stack.append(ret_string)
 
     def binary(self, operator):
+    	# (2,1,1)
         operand2 = self._python_stack.pop().short
         operand1 = self._python_stack.pop().short
         binary_string = EnglishByte('binary', operator, operand1, operand2) 
@@ -65,11 +72,29 @@ class FakeRun(object):
         self.english_stack.append(binary_string)
         
     def compare(self, operator):
+    	# (2,1,0)
     	# can be refactored with binary
         operand2 = self._python_stack.pop().short
         operand1 = self._python_stack.pop().short
         compare_string = EnglishByte('compare', operator, operand1, operand2) 
         self._python_stack.append(compare_string)
+        
+    def call_byte(self, command, arg):
+        # This is ugly
+        if command != 'call':
+            num_pop, num_stack, num_eng = self.tupledict[command]
+        else:
+            num_pop, num_stack, num_eng = (arg+1, 1, 1)
+    	pops = []
+    	for _ in range(num_pop):
+    	    pops.append(self._python_stack.pop().short)
+    	byte_string = EnglishByte(command, arg, *pops)
+    	# is for really necessary? 
+    	for _ in range(num_stack):
+    	    self._python_stack.append(byte_string)
+        for _ in range(num_eng):
+            self.english_stack.append(byte_string)    	
+    	
 
     def instructions(self):
         for line in self.disassembly:
