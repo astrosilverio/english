b4314d561dfc26aff62ec26dc44ef4082c3be4b0
Denis Garcia <denisgarci@gmail.com>
2013-11-22 10:45:48 -0500

    Tests for the "return" EnglishByte pass!

--- test_englishbyte.py
+++ test_englishbyte.py
@@ -3,24 +3,24 @@ import unittest
 
 class TestEnglishByte(unittest.TestCase):
 
-    fake_line_num = 0
+    fake_line_num = str(0)
 
 
     def test_return(self):
 
         return_1 = englishbyte.EnglishByte('return', self.fake_line_num, None, '1')
-        self.assertEqual('1', return_1.short)
+        self.assertEqual('Return 1', return_1.short)
         expected = '\t'.join((str(self.fake_line_num), 'Return 1.'))
         self.assertEqual(expected, return_1.full)
 
         return_x = englishbyte.EnglishByte('return', self.fake_line_num, None, 'a string')
-        self.assertEqual('a string', return_x.short)
-        expected = '\t'.join((str(self.fake_line_num), 'Retun a string.'))
+        self.assertEqual('Return a string', return_x.short)
+        expected = '\t'.join((str(self.fake_line_num), 'Return a string.'))
         self.assertEqual(expected, return_x.full)
 
         return_none = englishbyte.EnglishByte('return', self.fake_line_num, None, None)
-        self.assertEqual('None', return_none.short)
-        expected = '\t'.join((str(self.fake_line_num), 'Retun None.'))
+        self.assertEqual('Return None', return_none.short)
+        expected = '\t'.join((str(self.fake_line_num), 'Return None.'))
         self.assertEqual(expected, return_none.full)
 
 
