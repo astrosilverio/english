bebe61120a63a9dbc105acee7d73d09a675b55b9
Katie Silverio <astrosilverio@gmail.com>
2013-11-23 22:53:27 -0500

    Finished test_fakerun

--- test_fakerun.py
+++ test_fakerun.py
@@ -1,6 +1,7 @@
 import fakerun
 import unittest
 from englishbyte import EnglishByte
+from collections import deque
 
 class TestFakeRun(unittest.TestCase):
 
@@ -15,23 +16,6 @@ class TestFakeRun(unittest.TestCase):
         for command, arg in self.foo_fakerun.instructions():
            self.instructions_output.append((command, arg))
            
-        def call_byte_return(command, arg):
-            if command == 'call': # num_pop should be arg + 1
-                num_pop, num_stack, num_eng = (arg + 1, 1, 1)
-            else:
-                num_pop, num_stack, num_eng = self.foo_fakerun.tupledict[command]
-            pops = deque()
-            for _ in range(num_pop):
-                pops.appendleft(self.foo_fakerun._python_stack.pop().short)
-            byte_string = EnglishByte(command, self.line_num, arg, *pops) 
-            if num_stack:
-                self.foo_fakerun._python_stack.append(byte_string)
-            if num_eng:
-                self.english_stack.append(byte_string)           
-            return command, self.foo_fakerun.line_num, arg, pops
-            
-        self.call_byte_return(command, arg) = call_byte_return
-
     def test_instructions(self):
                
         self.assertEqual(('load','bar'), self.instructions_output[0])
@@ -46,37 +30,67 @@ class TestFakeRun(unittest.TestCase):
         expected = self.foo_fakerun.call_byte('load','bar')
         self.assertEqual(1, len(self.foo_fakerun._python_stack))
         self.assertEqual(0, len(self.foo_fakerun.english_stack))        
+        self.assertEqual('load', expected[0])
+        self.assertEqual('bar', expected[2])
+        self.assertEqual(deque([]), expected[3])
         
-        self.foo_fakerun.call_byte('load', 4)
+        expected = self.foo_fakerun.call_byte('load', 4)
         self.assertEqual(2, len(self.foo_fakerun._python_stack))
         self.assertEqual(0, len(self.foo_fakerun.english_stack))
+        self.assertEqual('load', expected[0])
+        self.assertEqual(4, expected[2])
+        self.assertEqual(deque([]), expected[3])
         
         self.foo_fakerun.call_byte('load', 3)
         self.foo_fakerun.call_byte('load', 'x')
         
-        self.foo_fakerun.call_byte('binary', 'add')
+        expected = self.foo_fakerun.call_byte('binary', 'add')
         self.assertEqual(3, len(self.foo_fakerun._python_stack))
         self.assertEqual(1, len(self.foo_fakerun.english_stack))
+        self.assertEqual('binary', expected[0])
+        self.assertEqual('add', expected[2])
+        self.assertEqual(deque(['3', 'x']), expected[3])
         
-        self.foo_fakerun.call_byte('call', 2)
+        expected = self.foo_fakerun.call_byte('call', 2)
         self.assertEqual(1, len(self.foo_fakerun._python_stack))
         self.assertEqual(2, len(self.foo_fakerun.english_stack))
+        self.assertEqual('call', expected[0])
+        self.assertEqual(2, expected[2])
+        self.assertEqual(deque(['bar', '4', '3 plus x']), expected[3])
         
-        self.foo_fakerun.call_byte('store', 'y')
+        expected = self.foo_fakerun.call_byte('store', 'y')
         self.assertEqual(0, len(self.foo_fakerun._python_stack))
         self.assertEqual(3, len(self.foo_fakerun.english_stack))
+        self.assertEqual('store', expected[0])
+        self.assertEqual('y', expected[2])
+        self.assertEqual(deque(['result of call to bar']), expected[3])
         
         self.foo_fakerun.call_byte('load', 'x')
         self.foo_fakerun.call_byte('load', 'y')
         
-        self.foo_fakerun.call_byte('compare', '>')
+        expected = self.foo_fakerun.call_byte('compare', '>')
         self.assertEqual(1, len(self.foo_fakerun._python_stack))
         self.assertEqual(3, len(self.foo_fakerun.english_stack))
+        self.assertEqual('compare', expected[0])
+        self.assertEqual('>', expected[2])
+        self.assertEqual(deque(['x', 'y']), expected[3])
         
-        self.foo_fakerun.call_byte('return', None)
+        expected = self.foo_fakerun.call_byte('return', None)
         self.assertEqual(0, len(self.foo_fakerun._python_stack))
         self.assertEqual(4, len(self.foo_fakerun.english_stack))
-    
+        self.assertEqual('return', expected[0])
+        self.assertEqual(None, expected[2])
+        self.assertEqual(deque(['x is greater than y']), expected[3])
+        
+    def test_run(self):
+        self.foo_fakerun.english_stack = []
+        self.foo_fakerun._python_stack = []
+        self.foo_fakerun.run()
+        
+        self.assertEqual('11\tCompute 3 plus x.', self.foo_fakerun.english_stack[0].full)
+        self.assertEqual('11\tCall bar with 4, 3 plus x as arguments.', self.foo_fakerun.english_stack[1].full)
+        self.assertEqual('11\tStore y as result of call to bar.', self.foo_fakerun.english_stack[2].full)
+        self.assertEqual('12\tReturn x is greater than y.', self.foo_fakerun.english_stack[3].full)
     
     
 if __name__ == '__main__':
