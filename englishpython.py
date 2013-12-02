4e6bab79289d220973fa6915d6940dc7471ebe4c
Katie Silverio <astrosilverio@gmail.com>
2013-12-02 17:44:47 -0500

    hey the tabs work. that's all. just tabs. no big deal. TABS TABS TABS

--- englishpython.py
+++ englishpython.py
@@ -14,44 +14,45 @@ class EnglishPython(object):
         self.disassembly = self.code_obj.code
         self.english_translation = map(lambda x: x.full, FakeRun(self.func).english_stack)
         self.create_num_tabs()
+        self.fix_end_else()
 
     def create_num_tabs(self):
         self.num_tabs = {}
         tabs = 0
+        tabs_stack = []
         first_line = self.func.func_code.co_firstlineno
         for line in self.disassembly:
             if str(line[0]) == 'SetLineno':
                 line_num = int(line[1]) - first_line + 1
             elif type(line[0]) == byteplay.Label:
-                self.num_tabs[line_num + 1] = tabs - 1
-            # this is not the right thing to do
-#            elif str(line[0]).startswith('JUMP_ABS'):
-#                tabs -= 1
-            elif type(line[1]) == byteplay.Label and not str(line[0]).startswith('JUMP_'):
+                tabs_stack.append(line[0])
+                self.num_tabs[line_num + 1] = tabs - (len(tabs_stack) - 1) // 2
+            elif str(line[0]).startswith('POP_JUMP'):
                 self.num_tabs[line_num] = tabs
+                tabs_stack.append(line[1])
                 tabs += 1
-            elif str(line[0]).startswith('JUMP_ABS'):
+            elif str(line[0]).startswith('JUMP'):
                 self.num_tabs[line_num] = tabs
+                tabs_stack.append(line[1])
             else:
                 self.num_tabs[line_num] = tabs
                 continue
 
-#     def create_num_tabs(self):
-#         self.num_tab = {}
-#         tabs = 0
-#         line_num = self.func.func_code.co_firstlineno + 1
-#         for line_num, line_text in self.english_translation:
-#             if line[0] != str(line_num):
-#                 line_num = line[0]
-#             if line_text.startswith('If') or line_text.startswith('Else'):
-#                 self.num_tab[line_num] = tabs
-#                 tabs += 1
+    def fix_end_else(self):
+        end_line = max(self.num_tabs.keys()) + self.func.func_code.co_firstlineno - 1
+        end_commands = [line for line in self.english_translation if int(line[0]) == end_line]
+        if len(end_commands) > 1 and (end_line, "Else:") in end_commands:
+            new_end = (end_line, ' '.join(["Finally,", end_commands[1:][0][1]]))
+            for line in end_commands:
+                self.english_translation.remove(line)
+            self.english_translation.append(new_end)
 
     def __str__(self):
         result = ''
+        first_line = self.func.func_code.co_firstlineno
         for line_num, line_text in self.english_translation:
             tabs = ' '
-            tabs += '\t' * self.num_tab[line_num]
+            tabs += '\t' * self.num_tabs[line_num - first_line + 1]
             line_num += self.func.func_code.co_firstlineno
             line_string = tabs.join((str(line_num), line_text))
             result = '\n'.join((result, line_string))
